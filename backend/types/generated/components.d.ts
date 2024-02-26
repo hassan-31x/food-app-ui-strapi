672c2f2618291bf7ff41c60e0dbc55d23d5e8ad8
Hassan <muhammadhassanchannel786@gmail.com>
2024-02-27 00:03:56 +0500

    feat: add navbar section in strapi

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,63 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ElementsButton extends Schema.Component {
+  collectionName: 'components_elements_buttons';
+  info: {
+    displayName: 'Button';
+    icon: '';
+  };
+  attributes: {
+    text: Attribute.String;
+    variant: Attribute.Enumeration<['FILLED', 'TRANSPARENT']>;
+    hasGlow: Attribute.Boolean & Attribute.DefaultTo<false>;
+    color: Attribute.String;
+  };
+}
+
+export interface SectionsHero extends Schema.Component {
+  collectionName: 'components_sections_heroes';
+  info: {
+    displayName: 'hero';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    videoUrl: Attribute.String;
+    overlayImage: Attribute.Media;
+    leftButton: Attribute.Component<'elements.button'>;
+    rightButton: Attribute.Component<'elements.button', true>;
+  };
+}
+
+export interface SectionsNavbar extends Schema.Component {
+  collectionName: 'components_sections_navbars';
+  info: {
+    displayName: 'navbar';
+  };
+  attributes: {
+    logo: Attribute.Media;
+  };
+}
+
+export interface SeoMetaData extends Schema.Component {
+  collectionName: 'components_seo_meta_data';
+  info: {
+    displayName: 'Meta Data';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.Text;
+    metaImage: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'elements.button': ElementsButton;
+      'sections.hero': SectionsHero;
+      'sections.navbar': SectionsNavbar;
+      'seo.meta-data': SeoMetaData;
+    }
+  }
 }
