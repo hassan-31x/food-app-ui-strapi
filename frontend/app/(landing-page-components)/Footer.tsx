369b7ec00d5446550cdd383249a09234b1b57b76
Hassan <muhammadhassanchannel786@gmail.com>
2024-03-06 23:42:06 +0500

    style: footer svg height fixed

--- frontend/app/(landing-page-components)/Footer.tsx
+++ frontend/app/(landing-page-components)/Footer.tsx
@@ -21,13 +21,14 @@ function Footer({}: Props) {
   };
 
   return (
-    <div className="h-[1.5%] w-full flex justify-center pb-8 md:pb-12 pt-10 relative">
+    <div className="h-[1.5%] w-full flex justify-center md:pb-12 pt-10 relative">
       <div className="custom-shape-divider-bottom-1709749424">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1200 120"
           preserveAspectRatio="none"
+          className="h-32 md:h-64 lg:h-96 xl:h-[550px]"
         >
           <path
             d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
